8d0b7b371061636753b36cf3e70807c386a6f2a5
Afnan Sayed <afnansayed1973@gimal.com>
2024-09-26 16:38:30 +0600

    card design

--- src/components/servicePage/serviceItem/ServiceItem.jsx
+++ src/components/servicePage/serviceItem/ServiceItem.jsx
@@ -3,6 +3,8 @@
 import useAxiosPublic from '@/hooks/useAxiosPublic/useAxiosPublic';
 import { useEffect, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
+import Image from 'next/image';
+import Link from 'next/link';
 
 
 
@@ -18,14 +20,32 @@ const ServiceItem = () => {
             return res.data;
         }
     })
-    // if(isLoading){
-    //     return <h1>Loading...</h1>
-    // }
+    if(isLoading){
+        return <h1>Loading...</h1>
+    }
 
-    console.log(services)
+    const data = (services.services)
     return (
-        <div>
-             
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+               {
+                data.map((service ) =>  <div 
+                className="card card-side bg-base-100 shadow-xl h-[250px]"
+                key={service._id}
+                service={service}
+                >
+                    <figure className='w-1/2'>
+                        <Image src={service.image} width={500} height={500} alt="service picture" className='w-full h-full'/>
+                    </figure>
+                    <div className="card-body w-1/2 bg-gradient-to-r from-sky-300 to-cyan-50">
+                      <h2 className="card-title text-gray-800">{service.name}</h2>
+                      {/* <p className='text-gray-600'>{service.description}</p> */}
+                      <p className='text-gray-600 font-semibold'>Price : {service.price} $</p> 
+                      <div className="card-actions justify-start">
+                        <Link href={`/${service._id}`} className="bg-gray-800 text-[#fff] rounded-lg px-6 py-2 hover:bg-sky-200 hover:text-gray-800 hover:scale-y-110  transition-all">View Details</Link>
+                      </div>
+                    </div>
+                  </div>)
+               }
         </div>
     );
 };
